3017c69de7419247cecf5f95bafcdbfade53471d
Moises Nantes <moisesnantes@gmail.com>
2023-04-07 07:00:34 -0400

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/moisesnantes/workshopmongodb/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.moisesnantes.workshopmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.moisesnantes.workshopmongodb.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
