1235ef9c5d0aaaab615e4e88b015392fb61d6b88
Moises Nantes <moisesnantes@gmail.com>
2023-04-10 16:34:35 -0400

    Obtendo um usuário por id

--- src/main/java/com/moisesnantes/workshopmongodb/services/UserService.java
+++ src/main/java/com/moisesnantes/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.moisesnantes.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.moisesnantes.workshopmongodb.domain.User;
 import com.moisesnantes.workshopmongodb.repository.UserRepository;
+import com.moisesnantes.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,13 @@ public class UserService {
 	private UserRepository repo;
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+		
 	}
 }
