cd60012a4b22ba853d51bee8b594c5fd2d5f75d6
Moises Nantes <moisesnantes@gmail.com>
2023-04-12 12:26:55 -0400

    Deleção de usuário com DELETE

--- src/main/java/com/moisesnantes/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/moisesnantes/workshopmongodb/resources/UserResource.java
@@ -41,5 +41,12 @@ public class UserResource {
 		
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 
 }
