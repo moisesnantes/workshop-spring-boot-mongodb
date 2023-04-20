28d6712c75ab298f1502592745749273abce966d
Moises Nantes <moisesnantes@gmail.com>
2023-04-19 21:22:05 -0400

    Obtendo um post por id

---
+++ src/main/java/com/moisesnantes/workshopmongodb/services/PostService.java
@@ -0,0 +1,31 @@
+package com.moisesnantes.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moisesnantes.workshopmongodb.domain.Post;
+import com.moisesnantes.workshopmongodb.repository.PostRepository;
+import com.moisesnantes.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+		
+	}
+	
+	
+}
