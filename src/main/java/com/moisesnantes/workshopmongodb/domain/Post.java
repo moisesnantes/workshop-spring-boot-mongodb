70c25a5c709cc247126daf1ffbc4fad46336f2bf
Moises Nantes <moisesnantes@gmail.com>
2023-04-20 20:55:33 -0400

    Acrescentando comentários aos posts

--- src/main/java/com/moisesnantes/workshopmongodb/domain/Post.java
+++ src/main/java/com/moisesnantes/workshopmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.moisesnantes.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.moisesnantes.workshopmongodb.dto.AuthorDTO;
+import com.moisesnantes.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
 
@@ -30,6 +35,8 @@ public class Post implements Serializable {
 		this.title = title;
 		this.body = body;
 		this.author = author;
+		
+		
 	}
 
 	public String getId() {
@@ -70,6 +77,13 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -87,6 +101,8 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 	
 
 }
